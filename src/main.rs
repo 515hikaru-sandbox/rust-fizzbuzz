8b8dd88151cf2f8ee553ff05635c3d2a6e59f795
515hikaru <12kojima.takahiro@gmail.com>
2018-09-06 23:41:31 +0900

    fizzbuzz

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    for n in 1..100 {
+        let s = fizzbuzz(n);
+        println!("{}", s);
+    }
+}
+
+fn fizzbuzz(n: i64) -> String {
+    if n % 15 == 0 {
+        return String::from("FizzBuzz");
+    };
+    if n % 5 == 0 {
+        return String::from("Buzz");
+    };
+    if n % 3 == 0 {
+        return String::from("Buzz");
+    };
+    n.to_string()
+}
